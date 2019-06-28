99b41282410601f6daebeadefc93cba86003b36a
Jonathan Dahan <hi@jonathan.is>
2019-06-28 19:06:20 -0400

    formatting

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ var caps = require('ssb-caps')
 var SSB = require('ssb-db')
 var caps = require('ssb-caps')
 
-//create a sbot with default caps. these can be overridden again when you call create.
+// create a sbot with default caps. these can be overridden again when you call create.
 function createSsbServer () {
   return SecretStack({ caps: { shs: Buffer.from(caps.shs, 'base64') } }).use(SSB)
 }
 module.exports = createSsbServer()
 
-//this isn't really needed anymore.
+// this isn't really needed anymore.
 module.exports.createSsbServer = createSsbServer
-
-
-
-
